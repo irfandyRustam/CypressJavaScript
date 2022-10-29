722193d09cc5d727545b86bf0e1ce76b4c717b6f
Irfandi Rustam <irfandy4hmad@gmail.com>
2022-10-29 18:38:00 +0800

    Part 11: Handling Tables

---
+++ cypress/e2e/Table.cy.js
@@ -0,0 +1,72 @@
+describe('Handle Tables', ()=>{
+
+  beforeEach('Login', ()=>{
+    cy.visit("https://demo.opencart.com/admin/index.php");
+    cy.get("#input-username").type("demo");
+    cy.get("#input-password").type("demo");
+    cy.get("button[type='submit']").click();
+
+    cy.get(".btn-close").click();
+
+    //Customers --> Customers
+    cy.get("#menu-customer>a").click(); //customers main menu
+    cy.get("#menu-customer>ul>li:first-child").click(); //customers sub-menu
+
+  })
+
+  it.skip('Check Number Rows & Columns', ()=>{
+    cy.get("#form-customer>>table>tbody>tr").should('have.length', '10');
+    cy.get("#form-customer>>table>thead>tr>td").should('have.length', '7');
+
+  })
+
+  it.skip('Check cell data from specific row & column', ()=>{
+    cy.get("#form-customer>>table>tbody>tr:nth-child(5)>td:nth-child(3)")
+      .contains("rs@yopmail.com");
+  })
+
+  it.skip('Read all the rows & columns data in the first page', ()=>{
+    cy.get("#form-customer>>table>tbody>tr")
+      .each(($row, index, $rows)=>{
+
+        cy.wrap($row).within(()=>{
+          cy.get("td").each(($col, index, $cols)=>{
+            cy.log($col.text());
+          })
+        })
+      })
+
+  })
+
+  it.only('Pagination', ()=>{
+    //Find total number of pages
+    /*let totalPages;
+    cy.get(".col-sm-6.text-end").then((e)=> {
+      let mytext = e.text();  //Showing 1 to 10 of 6113 (612 Pages)
+      totalPages = mytext.substring(mytext.indexOf("(")+1, mytext.indexOf("Pages")-1);
+      cy.log("Total number of pages in a table: " + totalPages);
+    })*/
+
+    let totalPages = 5;
+    for(let p=1; p<=totalPages; p++){
+      if(totalPages > 1){
+        cy.log("Active page is: " +p);
+        // cy.get("ul[class='pagination']>li:nth-child("+p+")").click();
+        cy.xpath("//ul[@class='pagination']//li//*[contains(text(),'"+p+"')]").click();
+        cy.wait(3000);
+
+        cy.get("#form-customer>>table>tbody>tr")
+          .each(($row, index, $rows)=>{
+            cy.wrap($row).within(()=>{
+              cy.get("td:nth-child(3)").then((e)=>{
+                cy.log(e.text()); //Email
+              })
+            })
+          })
+        
+      }
+    }
+
+  })
+
+})
