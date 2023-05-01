dbc58591467ebf6884d719337fae8674f65a204e
Irfandi Rustam <irfandy4work@gmail.com>
2023-05-01 16:46:19 +0800

    API Testing Part 1: HTTP Requests

---
+++ cypress/e2e/APITesting/HttpRequests.cy.js
@@ -0,0 +1,45 @@
+describe("HTTP Requests", () => {
+  it("GET Call", () => {
+    cy.request("GET", "https://jsonplaceholder.typicode.com/posts/1")
+      .its("status")
+      .should("equal", 200);
+  });
+
+  it("POST Call", () => {
+    cy.request({
+      method: "POST",
+      url: "https://jsonplaceholder.typicode.com/posts/",
+      body: {
+        title: "Test post",
+        body: "This is post call",
+        userID: 1,
+      },
+    })
+      .its("status")
+      .should("equal", 201);
+  });
+
+  it("PUT Call", () => {
+    cy.request({
+      method: "PUT",
+      url: "https://jsonplaceholder.typicode.com/posts/1",
+      body: {
+        title: "Test post - Updated",
+        body: "This is put call",
+        userID: 1,
+        id: 1,
+      },
+    })
+      .its("status")
+      .should("equal", 200);
+  });
+
+  it("DELETE Call", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://jsonplaceholder.typicode.com/posts/1",
+    })
+      .its("status")
+      .should("equal", 200);
+  });
+});
