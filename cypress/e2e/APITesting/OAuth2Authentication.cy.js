5fa635e230e228b0d9fb5522a06ebc6401540a17
irfandy <irfandy4hmad@gmail.com>
2023-06-04 20:37:52 +0800

    Separate github sensitive data into a fixtures file. Add those file in gitignore

--- cypress/e2e/APITesting/OAuth2Authentication.cy.js
+++ cypress/e2e/APITesting/OAuth2Authentication.cy.js
@@ -1,6 +1,6 @@
 // Pre-requisite: generate Auth code
 // https://github.com/login/oauth/authorize/{client_id}
-// Ex: https://github.com/login/oauth/authorize?client_id=ba9c12cfe3b961d24179
+// Ex: https://github.com/login/oauth/authorize?client_id=xxxxxxxxxxxxxxx
 
 /* 1) Get the OAuth2 access token
 POST: https://github.com/login/oauth/access_token
@@ -17,14 +17,21 @@ Auth: accessToken
 */
 describe("OAuth2", () => {
   let accessToken = "";
+  let githubData;
+  before(() => {
+    cy.fixture("githubAuth.json").then((data) => {
+      githubData = data;
+    });
+  });
+
   it("Get OAuth2 access token", () => {
     cy.request({
       method: "POST",
       url: "https://github.com/login/oauth/access_token",
       qs: {
-        client_id: "ba9c12cfe3b961d24179",
-        client_secret: "997f99805b3b39598418e72c998fdbacbf3fadb7",
-        code: "f53c3a644f27f6c11fb3",
+        client_id: githubData.client_id,
+        client_secret: githubData.client_secret,
+        code: githubData.code,
       },
     }).then((response) => {
       // access_token=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&scope=&token_type=bearer
